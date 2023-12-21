45f5999e62739868dfe104a286d675a60deecca4
luisfelipeas5 <luisfelipeas5@gmail.com>
2023-12-21 14:59:19 -0300

    feat: creates ComponentsDependenciesPrinter

---
+++ lib/src/presentation/printers/components_dependencies_printer.dart
@@ -0,0 +1,51 @@
+import 'package:clean_architecture_analysis/src/domain/entities/components/component.dart';
+import 'package:clean_architecture_analysis/src/domain/entities/components/component_dependency.dart';
+import 'package:clean_architecture_analysis/src/domain/entities/components/component_with_dependencies.dart';
+import 'package:clean_architecture_analysis/src/domain/use_cases/get_dependencies/get_components_with_dependencies.dart';
+import 'package:clean_architecture_analysis/src/presentation/printers/printer.dart';
+
+final class ComponentsDependenciesPrinter implements Printer {
+  final GetComponentsWithDependencies getComponentsWithDependencies;
+
+  const ComponentsDependenciesPrinter({
+    required this.getComponentsWithDependencies,
+  });
+
+  @override
+  Future<void> call() async {
+    final result = await getComponentsWithDependencies();
+    if (result.isFail()) {
+      return print("❌ Failed to get components with dependencies.");
+    }
+
+    print("✅ Components with dependencies:");
+    final componentsWithDependencies = result.data;
+    componentsWithDependencies?.forEach(_printComponentWithDependencies);
+  }
+
+  void _printComponentWithDependencies(
+    ComponentWithDependencies componentWithDependencies,
+  ) {
+    _printComponent(componentWithDependencies.component);
+
+    for (var dependency in componentWithDependencies.dependencies) {
+      _printDependency(dependency);
+    }
+  }
+
+  void _printDependency(ComponentDependency dependency) {
+    _printComponent(
+      dependency.component,
+      prefix: "        ",
+    );
+  }
+
+  void _printComponent(
+    Component component, {
+    String prefix = "",
+  }) {
+    final Component(:name, :type) = component;
+    final typeName = type?.name ?? "unknown";
+    print("$prefix|- Component $name ($typeName)");
+  }
+}
