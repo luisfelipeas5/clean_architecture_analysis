4f98fb91e4cc220271366adac0da7c5a5fda8cfb
luisfelipeas5 <luisfelipeas5@gmail.com>
2024-02-23 21:55:33 -0300

    feat: defines center and components cound for OrderCurcunference

--- lib/src/domain/entities/order/order_circuference.dart
+++ lib/src/domain/entities/order/order_circuference.dart
@@ -1,9 +1,54 @@
+import 'dart:math';
+
 class OrderCircuference {
   final int order;
   final double ratio;
+  final (double, double) center;
+  final int componentsCount;
 
   OrderCircuference({
     required this.order,
     required this.ratio,
+    required this.center,
+    required this.componentsCount,
   });
+
+  OrderCircuference copyWith({
+    double? ratio,
+    (double, double)? center,
+  }) {
+    return OrderCircuference(
+      order: order,
+      ratio: ratio ?? this.ratio,
+      center: center ?? this.center,
+      componentsCount: componentsCount,
+    );
+  }
+
+  double _nextAngle = 0;
+
+  (double, double) getNextComponentCoordinates() {
+    // Following: https://stackoverflow.com/a/839931/4756152
+    // x = cx + r * cos(a)
+    // y = cy + r * sin(a)
+    final (cx, cy) = center;
+    final r = ratio;
+
+    final a = _deg2rad(_nextAngle);
+    _incrementAngle();
+
+    final x = cx + (r * cos(a));
+    final y = cy + (r * sin(a));
+    return (x, y);
+  }
+
+  _incrementAngle() {
+    final length = 360;
+    final lengthForEachComponent = length / componentsCount;
+    _nextAngle = (_nextAngle + lengthForEachComponent) % length;
+  }
+
+  double _deg2rad(double deg) {
+    return deg / 180.0 * pi;
+  }
 }
--- lib/src/domain/use_cases/get_order_circuferences/get_order_circuferences.dart
+++ lib/src/domain/use_cases/get_order_circuferences/get_order_circuferences.dart
@@ -8,26 +8,56 @@ class GetOrderCircuferences {
   }) {
     final orders = <int, OrderCircuference>{};
 
+    double biggestRatio = 0;
+
+    componentsWithDependencies.sort(_sortComponents);
     for (var componentWithDeps in componentsWithDependencies) {
       final order = componentWithDeps.component.type?.order ?? -1;
-      final orderCircuference =
-          orders[order] ?? OrderCircuference(order: order, ratio: 0);
+      final orderCircuference = orders[order] ??
+          OrderCircuference(
+            order: order,
+            ratio: orders[order - 1]?.ratio ?? 0,
+            center: (0, 0),
+            componentsCount: 0,
+          );
       final newOrderCircuference = _incrementCircuference(orderCircuference);
+      if (newOrderCircuference.ratio > biggestRatio) {
+        biggestRatio = newOrderCircuference.ratio;
+      }
       orders[order] = newOrderCircuference;
     }
 
-    // TODO: adds sum order-1 ratio to order before return
+    final center = (biggestRatio, biggestRatio);
+    final keys = orders.keys.toList()..sort();
+    for (var key in keys) {
+      final orderCircuference = orders[key]!;
+      orders[key] = orderCircuference.copyWith(
+        center: center,
+      );
+    }
 
     return Result.success(orders);
+  }
+
+  int _sortComponents(
+    ComponentWithDependencies a,
+    ComponentWithDependencies b,
+  ) {
+    final aOrder = a.component.type?.order ?? -1;
+    final bOrder = b.component.type?.order ?? -1;
+    return aOrder - bOrder;
   }
 
   OrderCircuference _incrementCircuference(
     OrderCircuference orderCircuference,
   ) {
-    final newRatio = orderCircuference.ratio + 10;
+    final newRatio = orderCircuference.ratio + 40;
+    final newComponentsCount = orderCircuference.componentsCount + 1;
     return OrderCircuference(
       order: orderCircuference.order,
       ratio: newRatio,
+      center: (0, 0),
+      componentsCount: newComponentsCount,
     );
   }
 }
