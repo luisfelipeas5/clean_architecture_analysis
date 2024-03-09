2e1dc4519938e9b471415be270c562ba37920e6f
luisfelipeas5 <luisfelipeas5@gmail.com>
2024-03-09 11:01:43 -0300

    feat: uses getFile on GetComponentByRelativePath

--- lib/src/domain/use_cases/get_components/get_component_by_relative_path.dart
+++ lib/src/domain/use_cases/get_components/get_component_by_relative_path.dart
@@ -1,22 +1,54 @@
 import 'package:clean_architecture_analysis/src/architecture_core/result/result.dart';
+import 'package:clean_architecture_analysis/src/domain/entities/analysis_config/analysis_config.dart';
 import 'package:clean_architecture_analysis/src/domain/entities/components/component.dart';
 import 'package:clean_architecture_analysis/src/domain/entities/components/component_type.dart';
+import 'package:clean_architecture_analysis/src/domain/repositories/file_system/file_system_repository.dart';
+import 'package:clean_architecture_analysis/src/domain/use_cases/get_analysis_config/get_analysis_config.dart';
 
 class GetComponentByRelativePath {
-  Result<Component> call({
+  final FileSystemRepository fileSystemRepository;
+  final GetAnalysisConfig getAnalysisConfig;
+
+  GetComponentByRelativePath({
+    required this.getAnalysisConfig,
+    required this.fileSystemRepository,
+  });
+
+  Future<Result<Component>> call({
     required String relativePath,
     required List<ComponentType> types,
-  }) {
+  }) async {
+    final analysisConfigResult = await getAnalysisConfig();
+    if (analysisConfigResult.isFail()) return analysisConfigResult.parseFail();
+
+    final analysisConfig = analysisConfigResult.data!;
+    final fileResult = await fileSystemRepository.getFile(
+      rootPath: analysisConfig.path,
+      relativePath: _getNormalizedRelativePath(relativePath, analysisConfig),
+    );
+    if (fileResult.isFail()) return fileResult.parseFail();
+
     final type = _getType(relativePath, types);
     final name = _getName(relativePath, type);
 
     final component = Component(
       name: name,
       type: type,
-      appFiles: [],
+      appFiles: [
+        fileResult.data!,
+      ],
     );
 
     return Result.success(component);
+  }
+
+  String _getNormalizedRelativePath(
+    String relativePath,
+    AnalysisConfig analysisConfig,
+  ) {
+    return relativePath
+        .replaceFirst("${analysisConfig.packageName}/", "")
+        .replaceFirst("src/", "");
   }
 
   ComponentType? _getType(String relativePath, List<ComponentType> types) {
